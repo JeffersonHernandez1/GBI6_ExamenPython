c4860ef184cb6c6d809bd110135c7799f65f2928
Jefferon <jefferson.hernandez@est.ikiam.edu.ec>
2022-08-03 16:43:51 -0500

    Jefferson Hernandez ha realizado el ejercicio 1

--- miningscience1.py
+++ miningscience1.py
@@ -1,29 +1,32 @@
+from Bio import Entrez
+from Bio import SeqIO
+from Bio import GenBank
+import re
+    
 def download_pubmed (keyword):
     """
-    La palabra "keyword" es clave para nuestra búsqueda.
+    El siguiente comando permite buscar articulos en pubmed mediante palabras claves
     """
-    from Bio import Entrez
-    from Bio import SeqIO
-    from Bio import GenBank 
+     
     Entrez.email = 'jefferson.hernandez@est.ikiam.edu.ec'
-    handle = Entrez.esearch(db='pubmed',
-                        sort='relevance',
-                        retmax='200',
-                        retmode='xml',
-                        term=keyword)
-    results = Entrez.read(handle)
-    id_list = results["IdList"]
-    ids = ','.join(id_list)
-    Entrez.email = 'A.N.Other@example.com'
-    handle = Entrez.efetch(db='pubmed',
-                       retmode='text',
-                       id=ids)
-    lista_id = ids.split(",")
-    return (lista_id)
+    busq = Entrez.read(Entrez.esearch(db="pubmed", 
+                            term=keyword,
+                            usehistory="y"))
+    webenv = busq["WebEnv"]
+    query_key = busq["QueryKey"]
+    handle = Entrez.efetch(db="pubmed",
+                           rettype="medline", 
+                           retmode="text", 
+                           retstart=0,
+                           retmax=543, webenv=webenv, query_key=query_key)
+    data = handle.read()
+    dataexp = re.sub(r'\n\s{6}','', data)
+    return dataexp
+    
 
 def mining_pubs(tipo):
     """Docstring mining_pubs"""
     if tipo == "AD":
-        
+      a=9  
     
-    return 
+    return (a)
